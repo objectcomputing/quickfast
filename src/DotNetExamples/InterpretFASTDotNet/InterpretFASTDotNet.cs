0b947356c2343020c99d2fecef5d5769544a2e55
fields.trevor <fields.trevor@localhost>
2009-04-09 17:29:32 +0000

    ChangeLogTag:Thu Apr 09 17:27:35 UTC 2009  Trevor Fields  <fields_t@ociweb.com>

---
+++ src/DotNetExamples/InterpretFASTDotNet/InterpretFASTDotNet.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickFASTDotNet
+{
+    namespace Examples
+    {
+        public class InterpretFASTDotNet
+        {
+            string templateFileName_;
+            System.IO.FileStream templateFile_;
+            string fastFileName_;
+            System.IO.FileStream fastFile_;
+            string outputFileName_;
+            System.IO.FileStream outputFileStream_;
+            System.IO.TextWriter outputFile_;
+
+            bool resetOnMessage_;
+            bool strict_;
+            bool verboseExecution_;
+
+            public InterpretFASTDotNet()
+            {
+                templateFileName_ = null;
+                fastFileName_ = null;
+                outputFileName_ = null;
+                resetOnMessage_ = false;
+                strict_ = true;
+                verboseExecution_ = false;
+            }
+
+
+            public void usage()
+            {
+                System.Console.WriteLine("Usage of this program:");
+                System.Console.WriteLine("  -t file     : Template file (required)");
+                System.Console.WriteLine("  -f file     : FAST Message file (required)");
+                System.Console.WriteLine("  -p file     : File to which performance measurements are written. (default standard output)");
+                System.Console.WriteLine("  -r          : Toggle 'reset decoder on every message' (default false).");
+                System.Console.WriteLine("  -s          : Toggle 'strict decoding rules' (default true).");
+                System.Console.WriteLine("  -vx         : Toggle 'noisy execution progress' (default false).");
+            }
+
+
+
+            public bool init(ref string[] args)
+            {
+                bool readTemplateName = false;
+                bool readSourceName = false;
+                bool readOutName = false;
+                bool ok = true;
+
+                foreach (string opt in args)
+                {
+                    if (readTemplateName)
+                    {
+                        templateFileName_ = opt;
+                        readTemplateName = false;
+                    }
+                    else if (readSourceName)
+                    {
+                        fastFileName_ = opt;
+                        readSourceName = false;
+                    }
+                    else if (readOutName)
+                    {
+                        outputFileName_ = opt;
+                        readOutName = false;
+                    }
+                    else if (opt == "-r")
+                    {
+                        resetOnMessage_ = !resetOnMessage_;
+                    }
+                    else if (opt == "-s")
+                    {
+                        strict_ = !strict_;
+                    }
+                    else if (opt == "-t")
+                    {
+                        readTemplateName = true;
+                    }
+                    else if (opt == "-f")
+                    {
+                        readSourceName = true;
+                    }
+                    else if (opt == "-o")
+                    {
+                        readOutName = true;
+                    }
+                    else if (opt == "-h")
+                    {
+                        ok = false;
+                    }
+                    else if (opt == "-vx")
+                    {
+                        verboseExecution_ = !verboseExecution_;
+                    }
+                }
+
+                try
+                {
+                    if (templateFileName_ == null)
+                    {
+                        ok = false;
+                        System.Console.WriteLine("ERROR: -t [templatefile] option is required.");
+                    }
+                    if (ok)
+                    {
+                        try
+                        {
+
+                            templateFile_ = new System.IO.FileStream(templateFileName_, System.IO.FileMode.Open);
+                        }
+                        catch (System.IO.IOException iex)
+                        {
+                            ok = false;
+                            System.Console.WriteLine("ERROR: Can't open template file: {0}", templateFileName_);
+                            System.Console.WriteLine(iex.ToString());
+                        }
+                    }
+                    if (fastFileName_ == null)
+                    {
+                        ok = false;
+                        System.Console.WriteLine("ERROR: -f [FASTfile] option is required.");
+                    }
+                    if (ok)
+                    {
+                        try
+                        {
+                            fastFile_ = new System.IO.FileStream(fastFileName_, System.IO.FileMode.Open);
+                        }
+                        catch (System.IO.IOException iex)
+                        {
+                            ok = false;
+                            System.Console.WriteLine("ERROR: Can't open FAST Message file: {0}", fastFileName_);
+                            System.Console.WriteLine(iex.ToString());
+                        }
+                    }
+                    if (ok && outputFileName_ != null)
+                    {
+                        try
+                        {
+                            outputFileStream_ = new System.IO.FileStream(outputFileName_, System.IO.FileMode.OpenOrCreate);
+                            outputFile_ = new System.IO.StreamWriter(outputFileStream_);
+                        }
+
+                        catch (System.IO.IOException iex)
+                        {
+                            ok = false;
+                            System.Console.WriteLine("ERROR: Can't open performance output file: {0}", outputFileName_);
+                            System.Console.WriteLine(iex.ToString());
+                        }
+                    }
+                    else
+                    {
+                        outputFile_ = System.Console.Out;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    ok = false;
+                }
+
+                if (!ok)
+                {
+                    this.usage();
+                }
+                return ok;
+            }
+
+
+            public int run()
+            {
+                int result = 0;
+                try
+                {
+                    if (verboseExecution_)
+                    {
+                        System.Console.WriteLine("Parsing templates");
+                    }
+                    QuickFASTDotNet.Codecs.TemplateRegistry templateRegistry = QuickFASTDotNet.Codecs.TemplateRegistry.Parse(templateFile_);
+                    if (verboseExecution_)
+                    {
+                        System.Console.WriteLine("Parsed {0} templates.", templateRegistry.Size);
+                        System.Console.WriteLine("Decoding messages");
+                    }
+
+                    MessageInterpreter handler = new MessageInterpreter(outputFile_);
+                    QuickFASTDotNet.Codecs.SynchronousDecoder decoder = new QuickFASTDotNet.Codecs.SynchronousDecoder(templateRegistry, fastFile_);
+                    decoder.ResetOnMessage = resetOnMessage_;
+                    decoder.Strict = strict_;
+                    QuickFASTDotNet.Codecs.MessageReceivedDelegate handlerDelegate;
+                    handlerDelegate = new QuickFASTDotNet.Codecs.MessageReceivedDelegate(handler.MessageReceived);
+
+                    decoder.Decode(handlerDelegate);
+                    if (verboseExecution_)
+                    {
+                        System.Console.WriteLine("Decoded {0} messages.", decoder.MessageCount);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    result = 1;
+                }
+
+                return result;
+            }
+
+
+            
+            static int Main(string[] args)
+            {
+                int result = 0;
+                InterpretFASTDotNet app = new InterpretFASTDotNet();
+
+                if (app.init(ref args))
+                {
+                    result = app.run();
+                }
+                else
+                {
+                    System.Console.WriteLine("ERROR: Failed initialization.");
+                    result = 1;
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ src/DotNetExamples/InterpretFASTDotNet/MessageInterpreter.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using QuickFASTDotNet.Messages;
+
+
+
+namespace QuickFASTDotNet
+{
+    namespace Examples
+    {
+        public class MessageInterpreter
+        {
+            private ulong recordCount_ = 0;
+            private ulong indent_ = 0;
+            System.IO.TextWriter out_;
+
+            public MessageInterpreter(System.IO.TextWriter outputStream)
+            {
+                recordCount_ = 0;
+                indent_ = 0;
+                out_ = outputStream;
+            }
+
+
+            public bool MessageReceived(Message message)
+            {
+                recordCount_ += 1;
+                out_.Write("Record #{0} ",  recordCount_);
+                out_.Flush();
+                formatMessage(message);
+                out_.WriteLine();
+                return true;
+            }
+
+
+            void formatMessage(Message message)
+            {
+
+                formatFieldSet(message);
+            }
+
+
+            void formatFieldSet(FieldSet fieldset)
+            {
+
+                foreach (System.Collections.Generic.KeyValuePair<FieldIdentity, Field> pair in fieldset)
+                {
+                    FieldType type = pair.Value.FieldType;
+                    if (type == FieldType.Sequence)
+                    {
+                        formatSequence(pair.Key, pair.Value);
+                    }
+                    else if (type == FieldType.Group)
+                    {
+                        formatGroup(pair.Value);
+                    }
+                    else
+                    {
+                        out_.Write(" {0}[{1}]=", pair.Key.Name, pair.Key.Id);
+                        displayFieldValue(pair.Value);
+                    }
+                }
+            }
+
+
+            void newline()
+            {
+                out_.WriteLine();
+                for (ulong cnt = 0; cnt < indent_ * 2; cnt++)
+                {
+                    out_.Write(' ');
+                }
+            }
+
+
+            void formatSequence(FieldIdentity identity, Field field)
+            {
+
+                SequenceField sf = (SequenceField) field;
+                Sequence seq = sf.Value;
+                int count = seq.Count;
+
+                newline();
+                out_.Write(" {0}[{1}]=", identity.Name, identity.Id);
+                out_.Write("Sequence[{0}]", count);
+                ulong entryCount = 0;
+                ++indent_;
+                foreach (FieldSet fieldset in seq)
+                {
+                    newline();
+                    out_.Write("[{0}]: ", entryCount++);
+                    formatFieldSet(fieldset);
+                }
+                --indent_;
+                newline();            
+            }
+
+
+            void formatGroup(Field field)
+            {
+            }
+
+
+            void displayFieldValue(Field field)
+            {
+                switch(field.FieldType)
+                {
+                    case Messages.FieldType.Int32:
+                        {
+                            Int32Field val = (Int32Field)field;
+                            out_.Write(val.Value);
+                            break;
+                        }
+                    case Messages.FieldType.UInt32:
+                        {
+                            UInt32Field val = (UInt32Field)field;
+                            out_.Write(val.Value);
+                            break;
+                        }
+                    case Messages.FieldType.Int64:
+                        {
+                            Int64Field val = (Int64Field)field;
+                            out_.Write(val.Value);
+                            break;
+                        }
+                    case Messages.FieldType.UInt64:
+                        {
+                            UInt64Field val = (UInt64Field)field;
+                            out_.Write(val.Value);
+                            break;
+                        }
+                    case Messages.FieldType.Decimal:
+                        {
+                            DecimalField val = (DecimalField)field;
+                            out_.Write(val.Value.ToDouble());
+                            break;
+                        }
+                    case Messages.FieldType.AsciString:
+                        {
+                            AsciStringField val = (AsciStringField)field;
+                            out_.Write(val.Value);
+                            break;
+                        }
+                    case Messages.FieldType.UnicodeString:
+                        {
+                            UnicodeStringField val = (UnicodeStringField)field;
+                            out_.Write(val.Value);
+                            break;
+                        }
+                    case Messages.FieldType.ByteVector:
+                        {
+                            // todo: we probably should hex dump this
+                            ByteVectorField val = (ByteVectorField)field;
+                            out_.Write(System.Convert.ToString(val.Value));
+                            break;
+                        }
+                    case Messages.FieldType.Sequence:
+                        {
+                            out_.Write("sequence");
+                            break;
+                        }
+                    case Messages.FieldType.Group:
+                        {
+                            out_.Write("group");
+                            break;
+                        }
+                    default:
+                        {
+                            out_.Write("unknown field type");
+                            break;
+                        }
+                }
+
+            }
+
+        
+
+        }
+    }
+}
