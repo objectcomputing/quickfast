19e28fe8e73a788e8b5fe383ff3c5a215d0dea38
dale.wilson <dale.wilson@localhost>
2010-01-05 00:16:57 +0000

    ChangeLogTag: Tue Jan  5 00:09:42 UTC 2010  Dale Wilson  <wilsond@ociweb.com>

---
+++ src/Messages/ValueMessageBuilder_fwd.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2009, Object Computing, Inc.
+// All rights reserved.
+// See the file license.txt for licensing information.
+#ifdef _MSC_VER
+# pragma once
+#endif
+#ifndef VALUEMESSAGEBUILDER_FWD_H
+#define VALUEMESSAGEBUILDER_FWD_H
+namespace QuickFAST{
+  namespace Messages{
+    class ValueMessageBuilder;
+  }
+}
+#endif // VALUEMESSAGEBUILDER_FWD_H
