c7710a99de6440e1ad67aa110e748bba0ea58d09
dale.wilson <dale.wilson@localhost>
2011-10-25 16:28:42 +0000

    ChangeLogTag: Tue Oct 25 16:26:45 UTC 2011  Dale Wilson  <wilsond@ociweb.com

---
+++ src/Examples/Examples/CommandArgParser.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2009, Object Computing, Inc.
+// All rights reserved.
+// See the file license.txt for licensing information.
+#include <Examples/ExamplesPch.h>
+#pragma message("Warning:  CommandArgParser has moved to Application namespace.");
+#include <Application/CommandArgParser.cpp>
