9d0ebc6c2f36d085160bea2454d7afe23b6921d0
Jeff Schmitz <schmitzj@ociweb.com>
2013-06-03 12:21:50 -0500

    Rework environment to not overwrite preset values Modify MPC file so that it can be built debug (couldn't find Xerces) Allow const field ptr in Sequence constructor Added replaceField to FieldSet so field can be overwritten.

--- setup.sh
+++ setup.sh
@@ -3,18 +3,54 @@
 # QuickFAST depends on BOOST V 1.36.0 or later. (http://www.boost.org/)
 # QuickFAST depends on Xerces V3.0 or later. (http://xerces.apache.org/xerces-c/)
 # Customize this file by setting variables to suit your environment
-export QUICKFAST_ROOT=`pwd`
+SOURCE="${BASH_SOURCE[0]}"
+SOURCE_DIR=`dirname $SOURCE`
+export QUICKFAST_ROOT=`readlink -f $SOURCE_DIR`
 
-export MPC_ROOT=~/MPC
+if test "$MPC_ROOT" = ""
+then
+  export MPC_ROOT=~/MPC
+fi
 
-export BOOST_ROOT=~/boost/boost_1_38_0
-export BOOST_ROOT_LIB=$BOOST_ROOT/lib
-export BOOST_CFG=-gcc41-mt
-export BOOST_VERSION=boost-1_38
-export XERCES_ROOT=~/xerces/xerces-c-3.0.1
-export XERCES_LIBPATH=$XERCES_ROOT/src/.libs
-export XERCES_LIBNAME=xerces-c-3.0
-export XERCES_INCLUDE=$XERCES_ROOT/src
+if test "$BOOST_ROOT" = ""
+then
+  export BOOST_ROOT=~/boost/boost_1_38_0
+fi
+
+if test "$BOOST_ROOT_LIB" = ""
+then
+  export BOOST_ROOT_LIB=$BOOST_ROOT/lib
+fi
+
+if test "$BOOST_CFG" = ""
+then
+  export BOOST_CFG=-gcc41-mt
+fi
+
+if test "$BOOST_VERSION" = ""
+then
+  export BOOST_VERSION=boost-1_38
+fi
+
+if test "$XERCES_ROOT" = ""
+then
+  export XERCES_ROOT=~/xerces/xerces-c-3.0.1
+fi
+
+if test "$XERCES_LIBPATH" = ""
+then
+  export XERCES_LIBPATH=$XERCES_ROOT/src/.libs
+fi
+
+if test "$XERCES_LIBNAME" = ""
+then
+  export XERCES_LIBNAME=xerces-c-3.0
+fi
+
+if test "$XERCES_INCLUDE" = ""
+then
+  export XERCES_INCLUDE=$XERCES_ROOT/src
+fi
 
 
 export PATH=$QUICKFAST_ROOT/bin:$MPC_ROOT:$PATH
